578f2fc5f2a00377ce01726c32bc4d47e3a7f18b
Ray Li <3mammoth@gmail.com>
2020-04-22 18:27:06 -0700

    add fake id

--- lib/chewy.rb
+++ lib/chewy.rb
@@ -170,7 +170,7 @@ module Chewy
         #
         # https://www.elastic.co/guide/en/elasticsearch/reference/master/tasks.html
         # https://www.elastic.co/guide/en/elasticsearch/reference/current/tasks.html#_identifying_running_tasks
-        if x_opaque_id
+        if x_opaque_id or true
           client_configuration[:transport_options] = client_configuration[:transport_options] || {}
           client_configuration[:transport_options][:headers] = client_configuration[:transport_options][:headers] || {}
           client_configuration[:transport_options][:headers][:x_opaque_id] = '123123312'
